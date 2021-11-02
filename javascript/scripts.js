3b163d775d92b5027aec1a333d25ee3548f162c1
BenVos <ben.j.vos@gmail.com>
2021-11-02 15:56:59 +0100

    New: Contactformulier, Sitemap, pdf-folder

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -6,3 +6,26 @@ function openNav() {
   function closeNav() {
     Object.assign(mySidenav.style,{width:"0px", opacity:"0%"});
   }
+
+// POST form data to zapier on submit
+$(document).ready(function (e) {
+    $("#contactForm").submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            url: "https://hooks.zapier.com/hooks/catch/11214164/bhcheob/",
+            type: "post",
+            data: $("#contactForm").serialize(),
+            success: function () {
+                // Redirect to another success page
+                window.location = "../index.html";
+            }
+        });
+    });
+
+    // zet op elke input en textarea in de form een 'touched' class nadat de 
+    // gebruiker het element bezocht heeft
+    $('form input, form textarea').focus(function(e) {
+        e.target.classList.add('touched');
+    });
+});
+
